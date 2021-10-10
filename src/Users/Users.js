24d5e06d4e71327dda2e65ee0cbdc20fa396a95a
Karolis Kimtys <karolis_k_7@hotmail.com>
2021-10-10 23:24:26 +0100

    minor change

--- src/Users/Users.js
+++ src/Users/Users.js
@@ -10,7 +10,6 @@ import inactive from '../assets/statusInactive.svg';
 export default function Users() {
   const [isUsers, setIsUsers] = useState();
   const [isLoaded, setIsLoaded] = useState(false);
-
   const [isStatus, setIsStatus] = useState('all');
   const [isRerenderList, setIsRerenderList] = useState(false);
 
@@ -33,9 +32,11 @@ export default function Users() {
   const handleAlphabeticOrder = (e) => {
     e.preventDefault();
 
+    // Sorts the JSON objects in A-Z order
     e.target.value === 'az' &&
       isUsers.sort((a, b) => a.firstName.localeCompare(b.firstName));
 
+    // Sorts the JSON objects in Z-A order
     e.target.value === 'za' &&
       isUsers.sort((a, b) => b.firstName.localeCompare(a.firstName));
 
@@ -76,15 +77,39 @@ export default function Users() {
         <div className={styles.UserPanel}>
           {isLoaded &&
             Object.values(isUsers).map((values, key) => {
+              // Gets the user years of age from date parsed by "moment" library
               const userAge = moment(values.dateOfBirth)
                 .fromNow()
                 .split(' ')[0];
 
               return (
                 <div key={key}>
-                  {userAge > ageLimit && (
-                    <div>
-                      {isStatus === 'all' ? (
+                  {userAge > ageLimit &&
+                    (isStatus === 'all' ? (
+                      <div>
+                        <div className={styles.User}>
+                          <div className={styles.StatusIndicator}>
+                            {values.status === 'active' ? (
+                              <img src={active} alt="" />
+                            ) : values.status === 'away' ? (
+                              <img src={away} alt="" />
+                            ) : (
+                              <img src={inactive} alt="" />
+                            )}
+                          </div>
+
+                          <div className={styles.UserStatusWrapper}>
+                            <div className={styles.UserName}>
+                              {values.firstName} {values.lastName}
+                            </div>
+                            <div className={styles.UserStatus}>
+                              {values.status}
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    ) : (
+                      values.status === isStatus && (
                         <div>
                           <div className={styles.User}>
                             <div className={styles.StatusIndicator}>
@@ -107,34 +132,8 @@ export default function Users() {
                             </div>
                           </div>
                         </div>
-                      ) : (
-                        values.status === isStatus && (
-                          <div>
-                            <div className={styles.User}>
-                              <div className={styles.StatusIndicator}>
-                                {values.status === 'active' ? (
-                                  <img src={active} alt="" />
-                                ) : values.status === 'away' ? (
-                                  <img src={away} alt="" />
-                                ) : (
-                                  <img src={inactive} alt="" />
-                                )}
-                              </div>
-
-                              <div className={styles.UserStatusWrapper}>
-                                <div className={styles.UserName}>
-                                  {values.firstName} {values.lastName}
-                                </div>
-                                <div className={styles.UserStatus}>
-                                  {values.status}
-                                </div>
-                              </div>
-                            </div>
-                          </div>
-                        )
-                      )}
-                    </div>
-                  )}
+                      )
+                    ))}
                 </div>
               );
             })}
